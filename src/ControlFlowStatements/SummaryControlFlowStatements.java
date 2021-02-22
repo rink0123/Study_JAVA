9e21e59832a71cc032a71f55b77a2732f88d08de
rink0123 <rink0123@naver.com>
2021-02-22 21:19:01 +0900

    제어문 요약(Summary of Control Flow Statements) 추가

---
+++ src/ControlFlowStatements/SummaryControlFlowStatements.java
@@ -0,0 +1,28 @@
+package ControlFlowStatements;
+
+/**
+ * <h3>제어문 요약(Summary of Control Flow Statements)</h3>
+ * 
+ * <h4>참고 사이트</h4>
+ * <ul>
+ * <li><a href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/flowsummary.html">
+ * Java Tutorials > Language Basics > Control Flow Statements > Summary of Control Flow Statements</a></li>
+ * </ul>
+ * 
+ * @since JDK 11
+ * @version 1.0.0.RELEASE
+ * @author 강민성
+ */
+public class SummaryControlFlowStatements {
+	/* 
+	 * 1. if문은 모든 제어문 중에서 가장 기본이다.
+	 * 2. if문은 조건식이 true로 평가되는 경우에만 프로그램에서 초기화 블록 안에 명령문을 실행하도록 지시한다.
+	 * 3. if-else문은 if절이 false로 평가될 때 보조 실행 경로를 제공한다.
+	 * 4. if 및 if-else와는 달리 switch문은 가능한 실행 경로를 얼마든지 허용한다.
+	 * 5. while 및 do-while문은 특정 조건이 true일 때 초기화 블록 안에 명령문을 계속 실행한다.
+	 * 6. do-while과 while의 차이점은 do-while은 상단 대신 루프 하단에 있는 표현식을 평가한다는 것이다.
+	 *    따라서, do 블록 내의 명령문은 항상 최소한 한 번 실행된다.
+	 * 7. for문은 다양한 값 범위에서 반복할 수 있는 간단한 방법을 제공한다.
+	 * 8. 두 가지 형태가 있는데, 그 중 하나는 컬렉션과 배열을 순환하도록 설계되었다.
+	 */
+}

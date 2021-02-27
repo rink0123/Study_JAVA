7a7668c5b9ae79231ffe3b3026b44389bb99e786
rink0123 <rink0123@naver.com>
2021-02-27 23:38:36 +0900

    init commit

--- src/ClassandObjects/AccessModifier.java
+++ src/ClassandObjects/AccessModifier.java
@@ -1,10 +1,55 @@
 package ClassandObjects;
 
+/**
+ * <h3>접근 제어자(access modifier)</h3>
+ * 
+ * <h4>참고 사이트</h4>
+ * <ul>
+ * <li><a href="https://docs.oracle.com/javase/tutorial/java/javaOO/accesscontrol.html">
+ * Java Tutorials > Classes and Objects > Controlling Access to Members of a Class</a></li>
+ * <li><a href="http://tcpschool.com/java/java_modifier_accessModifier">TCP School > JAVA > 35) 접근 제어자</a></li>
+ * </ul>
+ * 
+ * @since JDK 11
+ * @version 1.0.0.RELEASE
+ * @author 강민성
+ */
 public class AccessModifier {
-
+	/*
+	 * 접근 제어자(access modifier)
+	 * 1. 객체 지향에서 정보 은닉(data hiding)이란 사용자가 굳이 알 필요가 없는 정보는 사용자로부터 숨겨야 한다는 개념.
+	 *    이렇게 함으로써 사용자는 언제나 최소한의 정보만으로 프로그램을 손쉽게 사용할 수 있게 된다.
+	 * 2. 접근 제어자를 사용하면 클래스 외부에서의 직접적인 접근을 허용하지 않는 멤버를 설정하여 정보 은닉을 구체화할 수 있다.
+	 * 3. 접근 제어자는 생략이 가능하며 생략했을 때는 자동으로 default임을 뜻한다.
+	 * 4. 접근 제어자 종류: public, protected, default, private
+	 * 
+	 * 
+	 * 접근 제어자의 접근 범위
+	 * 1. 접근 범위가 큰 것부터 작은 순으로 public > protected > default > private.
+	 * 2. 접근 제어자의 접근 범위표
+	 * 접근 제어자   같은 일반클래스  같은 자식클래스  같은 패키지  다른 패키지의 일반클래스  다른 패키지의 자식클래스
+	 * public             O                O            
+	 * protected          O
+	 * default            O
+	 * private            O
+	 * 
+	 * 
+	 * 접근 제어자 선택에 대한 팁
+	 * 1. 여러 범위에서 고의적으로 사용해야되는 상황이 아니고서는 private 사용을 지향한다.
+	 *    무조건 public으로 할 경우 프로그램 실행 시 의도치 않게 참조하게 될 수 있기 때문이다.
+	 * 2. public은 주로 상수에 사용하는 것을 지향한다. 프로그램 실행 시 상수의 값은 변경이 불가하며
+	 *    주로 여러 곳에서 값을 참조할 수 있게하는 용도로 사용하기 때문이다.
+	 * 
+	 * 
+	 * public 접근 제어자
+	 * 1. public 메소드를 통해서만 해당 객체의 private 멤버에 접근할 수 있다.
+	 *    따라서 public 메소드는 private 멤버와 프로그램 사이의 인터페이스 역할을 수행한다고 할 수 있다.
+	 * 
+	 * 
+	 * protected 접근 제어자
+	 * 1. protected 멤버는 부모 클래스에 대해서는 public 멤버처럼 취급되며, 외부에서는 private 멤버처럼 취급한다.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
 }

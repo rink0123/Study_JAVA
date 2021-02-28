72895a883860bf35dbf5fffd06bfd5219d746d3a
rink0123 <rink0123@naver.com>
2021-02-28 19:54:17 +0900

    클래스 및 객체 생성 및 사용 요약 정리 완료

---
+++ src/ClassandObjects/SummaryClassandObjects.java
@@ -0,0 +1,37 @@
+package ClassandObjects;
+
+/**
+ * <h3>클래스 및 객체 생성 및 사용 요약(Summary of Creating and Using Classes and Objects)</h3>
+ * 
+ * <h4>참고 사이트</h4>
+ * <ul>
+ * <li><a href="https://docs.oracle.com/javase/tutorial/java/javaOO/summaryclasses.html">
+ * Java Tutorials > Classes and Objects > Summary of Creating and Using Classes and Objects</a></li>
+ * </ul>
+ * 
+ * @since JDK 11
+ * @version 1.0.0.RELEASE
+ * @author 강민성
+ */
+public class SummaryClassandObjects {
+	/*
+	 * 1. 클래스 선언은 클래스 이름을 지정하고 클래스 본문을 중괄호({})로 묶는다.
+	 * 2. 클래스 이름 앞에 접근 제어자(Access Modifier)가 올 수 있다.
+	 * 3. 클래스 본문에는 클래스에 대한 필드(field), 메소드(method), 생성자(Constructor)가 포함된다.
+	 * 4. 클래스는 필드를 사용하여 상태 정보를 담고, 메소드를 사용하여 동작을 구현한다.
+	 * 5. 클래스의 새 인스턴스를 초기화하는 생성자는 클래스 이름을 사용하며 반환 유형이 없는 메소드이다.
+	 * 6. 클래스 및 구성원(필드, 메소드, 생성자)은 접근 제어자를 통해 접근 범위를 제어한다.
+	 * 7. static 키워드를 사용하여 클래스 변수 또는 클래스 메서드를 지정할 수 있다.
+	 * 8. 정적(static) 멤버로 선언되지 않은 멤버는 암시적으로 인스턴스 멤버이다.
+	 * 9. 클래스 변수는 클래스의 모든 인스턴스에서 공유되며 클래스 이름과 인스턴스 참조를 통해 접근할 수 있다.
+	 * 10. 클래스의 인스턴스는 인스턴스 참조를 통해 접근해야 하는 각 인스턴스 변수의 자체 복사본을 가져온다.
+	 * 11. new 연산자와 생성자를 사용하여 객체를 생성한다.
+	 * 12. 참조 변수에 생성된 객체를 할당하거나 직접 사용할 수 있다.
+	 * 13. 객체화 후 객체 참조 변수를 통해 해당 객체(객체화 된 클래스) 내에 있는 변수, 메소드를 호출할 수 있다.
+	 * 14. 객체 참조 변수를 통한 변수 호출: objectReference.variableName.
+	 * 15. 객체 참조 변수를 통한 메소드 호출: objectReference.methodName(argumentList) or objectReference.methodName().
+	 * 16. Garbage Collector는 사용되지 않은 객체를 자동으로 정리한다.
+	 * 17. 프로그램이 더 이상 참조할게 없는 객체는 사용되지 않는다.
+	 * 18. 객체 참조 변수의 값을 null로 할당하여 해당 값의 메모리 및 실제값을 삭제할 수 있다. 
+	 */
+}
